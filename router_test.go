4a5c8353618e81fabfab26623859e90e04252d64
mjin1220 <myeongjin1220@gmail.com>
2018-04-23 15:27:33 +0900

    without router_test.go

--- router_test.go
+++ router_test.go
@@ -1,16 +1,12 @@
 package main
 
 import (
-	"log"
-	"net/http"
 	"testing"
-
-	"github.com/julienschmidt/httprouter"
 )
 
 func TestIndex(t *testing.T) {
-	router := httprouter.New() // create router
-	router.GET("/", Index)     // GET Root
+	// router := httprouter.New() // create router
+	// router.GET("/", Index)     // GET Root
 
-	log.Fatal(http.ListenAndServe(":51234", router)) // 51234
+	// log.Fatal(http.ListenAndServe(":51234", router)) // 51234
 }
