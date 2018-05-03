faf4ce2afcf721f445c781aa56181b3e4e3391ac
mjin1220 <myeongjin1220@gmail.com>
2018-05-03 21:08:08 +0900

    add the TDD of Handler

--- handler_test.go
+++ handler_test.go
@@ -2,11 +2,37 @@ package main
 
 import (
 	"bytes"
+	"fmt"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 )
+
+func TestHandler(t *testing.T) {
+	srv := httptest.NewServer(Handler())
+	defer srv.Close()
+
+	res, err := http.Get(fmt.Sprintf("%s/", srv.URL))
+	if err != nil{
+		t.Fatalf("could not send GET request: %v", err)
+	}
+	
+	if res.StatusCode != http.StatusOK {
+		t.Fatalf("expected status OK; got %v", res.Status)
+	}
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		t.Fatalf("could not read response: %v", err)
+	}
+
+	data := string(bytes.TrimSpace(body))
+	if data != "StopBus" {
+		t.Fatalf("expected StopBus; got %v", data)
+	}
+
+}
 
 func TestIndexHandler(t *testing.T) {
 	req, err := http.NewRequest("GET", "localhost:51234/", nil)
@@ -29,7 +55,7 @@ func TestIndexHandler(t *testing.T) {
 	}
 
 	data := string(bytes.TrimSpace(body))
-	if data != "StopBus"{
+	if data != "StopBus" {
 		t.Fatalf("expected StopBus; got %v", data)
 	}
 }
