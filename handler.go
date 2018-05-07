180f9876890b65ab3b46c693cf77ff097aa8e745
mjin1220 <myeongjin1220@gmail.com>
2018-05-07 10:53:52 +0900

    fixed testing code

--- handler.go
+++ handler.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"os"
 
@@ -11,11 +13,30 @@ import (
 
 func Handler() http.Handler {
 	r := mux.NewRouter()
+
 	r.HandleFunc("/", GetOnly(IndexHandler))
+	r.HandleFunc("/driver/register", PostOnly(DriverRegisterHandler))
+
 	loggedRouter := handlers.LoggingHandler(os.Stdout, r)
 	return loggedRouter
 }
 
 func IndexHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "StopBus")
+}
+
+func DriverRegisterHandler(w http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close()
+
+	body, _ := ioutil.ReadAll(r.Body)
+	fmt.Println(string(body))
+
+	values := map[string]string{"result": "true", "errorCode": ""}
+	jsonValue, _ := json.Marshal(values)
+
+	fmt.Fprintln(w, string(jsonValue))
+}
+
+func SearchHandler(w http.ResponseWriter, r *http.Request) {
+
 }
