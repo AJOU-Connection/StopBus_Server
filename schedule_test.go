450d2d13ca397b7737ef0a8a6557434773aab077
mjin1220 <myeongjin1220@gmail.com>
2018-05-22 14:52:39 +0900

    without test code about go routine

--- schedule_test.go
+++ schedule_test.go
@@ -1,33 +1,27 @@
 package main
 
-import (
-	"net/http"
-	"sync"
-	"testing"
-)
+// func TestTargetObserver(t *testing.T) {
+// 	var wg sync.WaitGroup
 
-func TestTargetObserver(t *testing.T) {
-	var wg sync.WaitGroup
+// 	tt := []struct {
+// 		routeID    string
+// 		stationID  string
+// 		httpStatus int
+// 		resultCode int
+// 	}{
+// 		{"223000100", "203000066", http.StatusOK, 0},
+// 		{"234000026", "203000066", http.StatusOK, 0},
+// 		{"234000024", "203000066", http.StatusOK, 0},
+// 		{"200000053", "203000066", http.StatusOK, 0},
+// 		{"200000110", "203000066", http.StatusOK, 0},
+// 		{"200000112", "203000066", http.StatusOK, 0},
+// 		{"200000144", "203000066", http.StatusOK, 0},
+// 	}
+// 	for _, tc := range tt {
+// 		wg.Add(1)
+// 		defer wg.Done()
+// 		go TargetObserver(tc.routeID, tc.stationID)
+// 	}
 
-	tt := []struct {
-		routeID    string
-		stationID  string
-		httpStatus int
-		resultCode int
-	}{
-		{"223000100", "203000066", http.StatusOK, 0},
-		{"234000026", "203000066", http.StatusOK, 0},
-		{"234000024", "203000066", http.StatusOK, 0},
-		{"200000053", "203000066", http.StatusOK, 0},
-		{"200000110", "203000066", http.StatusOK, 0},
-		{"200000112", "203000066", http.StatusOK, 0},
-		{"200000144", "203000066", http.StatusOK, 0},
-	}
-	for _, tc := range tt {
-		wg.Add(1)
-		defer wg.Done()
-		go TargetObserver(tc.routeID, tc.stationID)
-	}
-
-	wg.Wait()
-}
+// 	wg.Wait()
+// }
