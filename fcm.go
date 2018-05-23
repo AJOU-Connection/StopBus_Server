5888d534bae85cab1548c8a72cb819192185395f
mjin1220 <myeongjin1220@gmail.com>
2018-05-23 21:24:30 +0900

    success fcm push

--- fcm.go
+++ fcm.go
@@ -1,10 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
 
-// Alert is a function
-func GetInAlert(routeID string, stationID string) {
-	fmt.Println("곧 버스가 도착합니다.")
-	fmt.Println("routeID:", routeID)
-	fmt.Println("stationID:", stationID)
+	fcm "github.com/NaySoftware/go-fcm"
+)
+
+// GetInAlert is a function
+func GetInAlert(routeID string, stationID string, tokens ...string) {
+	data := map[string]string{
+		"routeID":   routeID,
+		"stationID": stationID,
+	}
+
+	ids := []string{}
+
+	for _, token := range tokens {
+		ids = append(ids, token)
+	}
+
+	xds := []string{}
+
+	np := fcm.NotificationPayload{}
+	np.Title = "testTitle"
+	np.Body = "곧 버스가 도착합니다."
+	np.AndroidChannelID = "stopbus_danbk_mjin1220_notification_channel_id"
+	np.ClickAction = "OPEN_ACTIVITY_1"
+
+	c := fcm.NewFcmClient(config.ServerKey)
+	c.SetNotificationPayload(&np)
+	c.NewFcmRegIdsMsg(ids, data)
+	c.AppendDevices(xds)
+
+	status, err := c.Send()
+
+	if err == nil {
+		status.PrintResults()
+	} else {
+		fmt.Println(err)
+	}
 }
