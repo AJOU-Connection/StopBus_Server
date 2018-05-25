297f78899d11b7f107512382a18faa17749af7cd
mjin1220 <myeongjin1220@gmail.com>
2018-05-25 12:41:21 +0900

    change the path of log to log directory

--- log.go
+++ log.go
@@ -6,7 +6,13 @@ import (
 )
 
 func GetLogFile() *os.File {
-	f, err := os.OpenFile(fmt.Sprintf("access_.log"), os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0666)
+	logDirPath := "log"
+
+	if _, err := os.Stat(logDirPath); os.IsNotExist(err) {
+		os.Mkdir(logDirPath, os.ModeDir)
+	}
+
+	f, err := os.OpenFile(fmt.Sprintf("%v/access_.log", logDirPath), os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0666)
 	if err != nil {
 		panic(err)
 	}
