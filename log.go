a6ae3c961022ee336e7f7f119b30e468189bf944
mjin1220 <myeongjin1220@gmail.com>
2018-06-04 22:36:02 +0900

    add ErrorLogger

--- log.go
+++ log.go
@@ -3,18 +3,38 @@ package main
 import (
 	"fmt"
 	"os"
+	"time"
 )
 
-func GetLogFile() *os.File {
-	logDirPath := "logs"
+const logDirPath = "logs"
 
+func GetLogFile() *os.File {
 	if _, err := os.Stat(logDirPath); os.IsNotExist(err) {
 		os.Mkdir(logDirPath, os.ModeDir)
 	}
 
-	f, err := os.OpenFile(fmt.Sprintf("%v/access_.log", logDirPath), os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0666)
+	f, err := os.OpenFile(fmt.Sprintf("%v/access.log", logDirPath), os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0666)
 	if err != nil {
 		panic(err)
 	}
 	return f
+}
+
+func ErrorLogger(err error) {
+	if _, err := os.Stat(logDirPath); os.IsNotExist(err) {
+		os.Mkdir(logDirPath, os.ModeDir)
+	}
+
+	f, err := os.OpenFile(fmt.Sprintf("%v/error.log", logDirPath), os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		panic(err)
+	}
+	f.WriteString(fmt.Sprintf("%v %v\n", getCurrentTime(), err.Error()))
+
+	return
+}
+
+func getCurrentTime() string {
+	t := time.Now()
+	return fmt.Sprintf("%02d-%02d-%02d %02d:%02d:%02d", t.Year()%100, t.Month(), t.Day(), t.Hour(), t.Minute(), t.Second())
 }
