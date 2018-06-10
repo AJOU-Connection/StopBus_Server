142ea8166b6f4e2965ee7a5c15cea237ba8d6009
mjin1220 <myeongjin1220@gmail.com>
2018-06-11 04:59:49 +0900

    modify getOut algorithm

--- schedule.go
+++ schedule.go
@@ -10,12 +10,36 @@ import (
 func isTargetBusPassed(routeID string, stationID string, plateNo string) {
 	var currentPlateNo string
 
-	if currentPlateNo = getFirstPlateNo(routeID, stationID); currentPlateNo == plateNo {
+	if currentPlateNo = getFirstPlateNo(routeID, stationID); currentPlateNo[len(currentPlateNo)-4:] == plateNo {
+		fmt.Println("처음으로 도착하는 버스랑 같음")
+		fmt.Println("DB에 Driver를 추가")
 		addDriverStop(StopInput{routeID, stationID}, GetOff)
-		isBusPassed(routeID, stationID)
-		deleteGetOut(routeID, stationID, plateNo)
+		fmt.Println("버스가 지나가는지 계속 체크 시작")
+		isGetOutBusPassed(routeID, stationID, plateNo)
+		fmt.Println("종료")
 		return
 	}
+	fmt.Println("처음으로 도착하는 버스랑 다름- 다음 버스를 기다림")
+	stop := interval.Start(func() {
+		currentPlateNo = getFirstPlateNo(routeID, stationID)
+	}, 10*time.Second)
+
+	ticker := time.Tick(5 * time.Second)
+	for range ticker {
+		if currentPlateNo[len(currentPlateNo)-4:] == plateNo {
+			fmt.Println("DB에 Driver를 추가")
+			addDriverStop(StopInput{routeID, stationID}, GetOff)
+			fmt.Println("버스가 지나가는지 계속 체크 시작")
+			isGetOutBusPassed(routeID, stationID, plateNo)
+			fmt.Println("종료")
+			stop()
+		}
+	}
+}
+
+func isGetOutBusPassed(routeID string, stationID string, plateNo string) {
+	var prePlateNo, currentPlateNo string
+	isAlert := false
 
 	stop := interval.Start(func() {
 		currentPlateNo = getFirstPlateNo(routeID, stationID)
@@ -23,12 +47,27 @@ func isTargetBusPassed(routeID string, stationID string, plateNo string) {
 
 	ticker := time.Tick(5 * time.Second)
 	for range ticker {
-		if currentPlateNo == plateNo {
-			addDriverStop(StopInput{routeID, stationID}, GetOff)
-			isBusPassed(routeID, stationID)
-			deleteGetOut(routeID, stationID, plateNo)
-			stop()
+		if prePlateNo == "" {
+			prePlateNo = currentPlateNo
+			continue
 		}
+
+		arrivalItem := GetBusArrivalOnlyOne(routeID, stationID)
+		if (arrivalItem.PredictTime1 <= 2) && (!isAlert) {
+			fmt.Println("2분미만으로 사용자에게 알림이 감")
+			isAlert = true
+			GetOutAlert(routeID, stationID, plateNo)
+			fmt.Println("2분미만으로 사용자를 DB에서 지움")
+			deleteGetOut(routeID, stationID, plateNo[len(plateNo)-4:])
+		}
+
+		if prePlateNo != currentPlateNo {
+			fmt.Println("버스가 지나가서 DB에서 정차여부를 삭제함")
+			deleteDriverStop(routeID, stationID)
+			stop()
+			break
+		}
+		fmt.Println("체크중")
 	}
 }
 
@@ -48,7 +87,6 @@ func isBusPassed(routeID string, stationID string) {
 
 		if prePlateNo != currentPlateNo {
 			deleteDriverStop(routeID, stationID)
-			fmt.Println("isBusPassed end")
 			stop()
 			break
 		}
