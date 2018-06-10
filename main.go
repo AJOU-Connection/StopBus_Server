0b968726bf7dcbde739a17c2b81f7beab60a06cd
mjin1220 <myeongjin1220@gmail.com>
2018-06-11 05:00:06 +0900

    add fake bus in system

--- main.go
+++ main.go
@@ -4,9 +4,12 @@ import "log"
 
 // main is the main function.
 func main() {
+	go fakeBus.Run()
+
 	server := Server{}
 	err := server.Run(51234)
 	if err != nil {
 		log.Fatalf("main error: %v", err)
 	}
+
 }
